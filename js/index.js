e1fd9e241c0cb79d8deeaaffcc27a2b900bcd64e
Anthony Cho <antcho92@gmail.com>
2016-04-25 14:52:07 -0700

    Added ajax call for experimentation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+	$.ajax({
+		url: 'https://api.twitch.tv/kraken/streams/wagamamatv',
+		type: 'GET',
+		dataType: 'jsonp',
+		success: function(data) {
+			$('#fccChannel').append("<img src=\'"+data.stream.preview.medium+"\'>");
 
+		}
+	});
 });
