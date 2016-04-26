3dd8f88bd8559c872283f2f2f593576107dde0f5
Anthony Cho <antcho92@gmail.com>
2016-04-26 16:50:04 -0700

    Added some more formatting for organizing channel data to page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
+
+	var streams = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
+
 	$.ajax({
-		url: 'https://api.twitch.tv/kraken/streams/wagamamatv',
+		url: 'https://api.twitch.tv/kraken/streams/ESL_SC2',
 		type: 'GET',
 		dataType: 'jsonp',
 		success: function(data) {
-			$('#fccChannel').append("<img src=\'"+data.stream.preview.medium+"\'>");
-
+			var channelLogo = data.stream.channel.logo,
+				channelUrl = data.stream.channel.url,
+				channelName = data.stream.channel.name,
+				channelGame = data.stream.channel.game;
+			var html = "<div class='row'> <div class='col-sm-1'><img src='"+data.stream.channel.logo+"'>
+				<div class='col-sm-2'> <h3>" + channelName + "</h3> </div> </div>";
+			$('#fccChannel').append(html);
+			$('#fccChannel').append("<img src='"+data.stream.channel.logo+"'> <a href=\'" + data.stream.channel.url +
+				"'> <h3>" + data.stream.channel.name + "</h3> </a> <br> <p>" + data.stream.channel.game + "</p>");
 		}
 	});
 });
