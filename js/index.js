37f783f4cad8200cadd85f88b9e9ec1d9ba12090
Anthony Cho <antcho92@gmail.com>
2016-04-27 15:37:20 -0700

    Creates a row for each channel in array

--- js/index.js
+++ js/index.js
@@ -1,32 +1,39 @@
-var streams = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
+var channels = ["FreeCodeCamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas", "beohoff", "WagamamaTV", "brunofin", "sing_sing", "Arteezy"];
 
-$.ajax({
-	url: 'https://api.twitch.tv/kraken/streams/ESL_SC2',
-	type: 'GET',
-	dataType: 'jsonp',
-	success: function(data) {
-		var game,
-			status,
-			description,
-			logo = data.stream.channel.logo,
-			url = data.stream.channel.url,
-			channelName = data.stream.channel.name;
-    if (data.stream === null) {
-      game = "Offline";
-      status = "offline";
-    } else if (data.stream === undefined) {
-      game = "Account Closed";
-      status = "offline";
-    } else {
-      game = data.stream.game;
-      status = "online";
-			description = data.stream.channel.status;
-    };
-		var html = "<div class='row online'> <div class='col-xs-1'><img class='logo' src='" + logo +
-			"'> </div> <div class='col-xs-2'> <a href='" + url + "'>" + channelName + "</a></div> <div class='col-xs-8'>" + game +
-			"<span class='hidden-xs'>: " + description + "</span> </div> </div>";
-		$('#fccChannel').append(html);
-	}
+channels.forEach(function(channel) {
+	$.ajax({
+		url: 'https://api.twitch.tv/kraken/streams/' + channel,
+		type: 'GET',
+		dataType: 'jsonp',
+		success: function(data) {
+			var game,
+				status,
+				description,
+				logo,
+				url = 'https://www.twitch.tv/' + channel,
+				channelName = channel;
+	    if (data.stream === null) {
+	      game = "Offline";
+	      status = "offline";
+				logo = 'images/offline.jpg';
+				description = "";
+	    } else if (data.stream === undefined) {
+	      game = "Account Closed";
+	      status = "offline";
+				logo = 'images/offline.jpg';
+				description = "";
+	    } else {
+	      game = data.stream.game;
+	      status = "online";
+				description = ": " + data.stream.channel.status;
+				logo = data.stream.channel.logo;
+	    };
+			var html = "<div class='row " + status + "'> <div class='col-xs-1'><img class='logo' src='" + logo +
+				"'> </div> <div class='col-xs-3'> <a href='" + url + "'>" + channelName + "</a></div> <div class='col-xs-8'>" + game +
+				"<span class='hidden-xs'>" + description + "</span> </div> </div>";
+			status === "online" ? $('#channels').prepend(html) : $('#channels').append(html);
+		}
+	});
 });
 
 $(document).ready(function() {
