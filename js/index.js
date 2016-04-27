efbef538a7219919b89cd2200677b7f7065d6dfb
Anthony Cho <antcho92@gmail.com>
2016-04-26 17:03:58 -0700

    Trying to figure out why bootstrap isn't working for jquery added things

--- js/index.js
+++ js/index.js
@@ -11,11 +11,11 @@ $(document).ready(function() {
 				channelUrl = data.stream.channel.url,
 				channelName = data.stream.channel.name,
 				channelGame = data.stream.channel.game;
-			var html = "<div class='row'> <div class='col-sm-1'><img src='"+data.stream.channel.logo+"'>
-				<div class='col-sm-2'> <h3>" + channelName + "</h3> </div> </div>";
+			var html = "<div class='row'> <div class='col-sm-1'><img src='" + data.stream.channel.logo +
+				"'> </div> <div class='col-sm-2'> <h3>" + channelName + "</h3> </div> </div>";
 			$('#fccChannel').append(html);
-			$('#fccChannel').append("<img src='"+data.stream.channel.logo+"'> <a href=\'" + data.stream.channel.url +
-				"'> <h3>" + data.stream.channel.name + "</h3> </a> <br> <p>" + data.stream.channel.game + "</p>");
+			//$('#fccChannel').append("<img src='"+data.stream.channel.logo+"'> <a href=\'" + data.stream.channel.url +
+				//"'> <h3>" + data.stream.channel.name + "</h3> </a> <br> <p>" + data.stream.channel.game + "</p>");
 		}
 	});
 });
